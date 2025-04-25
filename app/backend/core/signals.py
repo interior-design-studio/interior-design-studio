02ce68f6601958606d970660b52a08d6d56c700d
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-25 17:41:30 +0300

    Create signals for ProjectImage model and register in apps.py

--- app/backend/core/signals.py
+++ app/backend/core/signals.py
@@ -17,3 +17,18 @@ def delete_main_image_on_update(sender, instance, **kwargs):
     old_instance = Project.objects.get(id=instance.pk)
     if old_instance.main_image:
         old_instance.main_image.delete(save=False)
+
+
+@receiver(post_delete, sender=ProjectImage)
+def delete_gallery_image_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(save=False)
+
+
+@receiver(pre_save, sender=ProjectImage)
+def delete_gallery_image_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+    old_instance = ProjectImage.objects.get(id=instance.pk)
+    if old_instance.image:
+        old_instance.image.delete(save=False)
