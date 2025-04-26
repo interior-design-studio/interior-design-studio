bdf512571a2e23f25df17aa72b6b599554e304e8
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-26 17:54:07 +0300

    Register Article model in admin panel

--- app/backend/blog/admin.py
+++ app/backend/blog/admin.py
@@ -1,9 +1,31 @@
+from nested_admin import nested
+
 from django.contrib import admin
+from django.utils.html import format_html
 
-from blog.models import Article, ArticleComponent, ComponentAdvantage, ComponentFeature
+from blog.models import Article
+from blog.forms import ArticleAdminForm
 
 
-admin.site.register(Article)
-admin.site.register(ArticleComponent)
-admin.site.register(ComponentAdvantage)
-admin.site.register(ComponentFeature)
+# admin.site.register(ArticleComponent)
+# admin.site.register(ComponentAdvantage)
+# admin.site.register(ComponentFeature)
+
+
+@admin.register(Article)
+class ArticleAdmin(nested.NestedModelAdmin):
+    form = ArticleAdminForm
+    readonly_fields = ("preview",)
+
+    list_display = ("title",)
+    search_fields = ("title",)
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;" />',
+                obj.image.url
+            )
+        return "-"
+
+    preview.short_description = "Preview"
