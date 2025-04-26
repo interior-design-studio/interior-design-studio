930b52b5586a7fe12b4d89443592bfb96cc0c983
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-26 16:29:36 +0300

    Register Project model in admin panel

--- app/backend/core/admin.py
+++ app/backend/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
+from core.forms import ProjectAdminForm
 from core.models import (
     Tag,
     Project,
@@ -12,9 +14,28 @@ from core.models import (
 
 
 admin.site.register(Tag)
-admin.site.register(Project)
 admin.site.register(ProjectStyle)
 admin.site.register(ProjectImage)
 admin.site.register(Service)
 admin.site.register(ProjectConfiguration)
 admin.site.register(Consultation)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    form = ProjectAdminForm
+    readonly_fields = ("preview",)
+
+    list_display = ("name",)
+    list_filter = ("style", "tags")
+    search_fields = ("name",)
+
+    def preview(self, obj):
+        if obj.main_image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;" />',
+                obj.main_image.url
+            )
+        return "-"
+
+    preview.short_description = "Preview"
