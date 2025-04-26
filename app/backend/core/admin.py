c5815ae994641c1d57c62542b9d75ae8ff63a5cb
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-26 16:32:28 +0300

    Create ImageInline and add to ProjectAdmin

--- app/backend/core/admin.py
+++ app/backend/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from core.forms import ProjectAdminForm
+from core.forms import ProjectAdminForm, ProjectImageAdminForm
 from core.models import (
     Tag,
     Project,
@@ -15,15 +15,32 @@ from core.models import (
 
 admin.site.register(Tag)
 admin.site.register(ProjectStyle)
-admin.site.register(ProjectImage)
 admin.site.register(Service)
 admin.site.register(ProjectConfiguration)
 admin.site.register(Consultation)
 
 
+class ImageInline(admin.TabularInline):
+    model = ProjectImage
+    form = ProjectImageAdminForm
+    extra = 1
+    readonly_fields = ("preview",)
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;" />',
+                obj.image.url
+            )
+        return "-"
+
+    preview.short_description = "Preview"
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     form = ProjectAdminForm
+    inlines = (ImageInline,)
     readonly_fields = ("preview",)
 
     list_display = ("name",)
