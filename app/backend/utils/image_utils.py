abda308cd55251051a86e41de5f6540cc128390b
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-27 16:09:00 +0300

    Create NewImageOptimizationFormMixin to optimize forms implementation

--- app/backend/utils/image_utils.py
+++ app/backend/utils/image_utils.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 from io import BytesIO
 
+from django import forms
 from django.core.files.base import ContentFile
 from django.db.models.fields.files import ImageFieldFile
 
@@ -35,3 +36,23 @@ class OldImageDeletionMixin:
             old_path = old_image.path
             if os.path.isfile(old_path):
                 os.remove(old_path)
+
+
+class NewImageOptimizationFormMixin(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._old_image_name = None
+
+    def optimize_new_image(self, field_name: str):
+        img = getattr(self.instance, field_name, None)
+        self._old_image_name = img.name if img else None
+
+        new_image = self.cleaned_data.get(field_name)
+
+        old_name = self._old_image_name.split(".")[0] if self._old_image_name else None
+        new_name = new_image.name.split(".")[0] if new_image else None
+
+        if new_name and new_name != old_name:
+            return optimize_image_to_webp(new_image)
+
+        return new_image
