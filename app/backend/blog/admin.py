7baaa22e185e348fd07b7d04535503d078539e9f
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-27 17:43:51 +0300

    Update admin.py in blog app

--- app/backend/blog/admin.py
+++ app/backend/blog/admin.py
@@ -1,36 +1,31 @@
 from nested_admin import nested
 
 from django.contrib import admin
-from django.utils.html import format_html
 
 from blog.models import Article, ArticleComponent, ComponentAdvantage, ComponentFeature
 from blog.forms import ArticleAdminForm, ArticleComponentAdminForm
+from utils.image_utils import preview_display
 
 
 class ComponentAdvantageInline(nested.NestedTabularInline):
     model = ComponentAdvantage
-    extra = 1
+    extra = 0
 
 
 class ComponentFeatureInline(nested.NestedTabularInline):
     model = ComponentFeature
-    extra = 1
+    extra = 0
 
 
 class ComponentInline(nested.NestedStackedInline):
     model = ArticleComponent
     form = ArticleComponentAdminForm
-    extra = 1
+    extra = 0
     readonly_fields = ("preview",)
     inlines = (ComponentAdvantageInline, ComponentFeatureInline)
 
     def preview(self, obj):
-        if obj.image:
-            return format_html(
-                '<img src="{}" style="max-height: 200px;" />',
-                obj.image.url
-            )
-        return "-"
+        return preview_display(obj, "image")
 
     preview.short_description = "Preview"
 
@@ -45,11 +40,6 @@ class ArticleAdmin(nested.NestedModelAdmin):
     search_fields = ("title",)
 
     def preview(self, obj):
-        if obj.image:
-            return format_html(
-                '<img src="{}" style="max-height: 200px;" />',
-                obj.image.url
-            )
-        return "-"
+        return preview_display(obj, "image")
 
     preview.short_description = "Preview"
