dbb9c3bd6b2cbe01d51cb9a2972a9b2beab3d224
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-27 17:45:33 +0300

    Update admin.py in core app

--- app/backend/core/admin.py
+++ app/backend/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.utils.html import format_html
 
 from core.forms import ProjectAdminForm, ProjectImageAdminForm
+from utils.image_utils import preview_display
 from core.models import (
     Tag,
     Project,
@@ -27,12 +27,7 @@ class ImageInline(admin.TabularInline):
     readonly_fields = ("preview",)
 
     def preview(self, obj):
-        if obj.image:
-            return format_html(
-                '<img src="{}" style="max-height: 200px;" />',
-                obj.image.url
-            )
-        return "-"
+        return preview_display(obj, "image")
 
     preview.short_description = "Preview"
 
@@ -48,11 +43,6 @@ class ProjectAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
     def preview(self, obj):
-        if obj.main_image:
-            return format_html(
-                '<img src="{}" style="max-height: 200px;" />',
-                obj.main_image.url
-            )
-        return "-"
+        return preview_display(obj, "main_image")
 
     preview.short_description = "Preview"
