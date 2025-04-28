1cd4e32ca3b83bd49fb0b406ea1c763efec6f7a5
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-28 14:52:30 +0300

    Create Question model in consultations app

--- app/backend/consultations/models.py
+++ app/backend/consultations/models.py
@@ -1,1 +1,13 @@
+from django.db import models
 
+
+class Question(models.Model):
+    order = models.PositiveIntegerField()
+    text = models.CharField(max_length=255)
+    is_protected = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self) -> str:
+        return self.text
