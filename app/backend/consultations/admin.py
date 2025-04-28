179c4cf8d14f0962b2a291ab6b142f0a62fd0e96
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-28 15:15:31 +0300

    Register consultations app models in admin panel

--- app/backend/consultations/admin.py
+++ app/backend/consultations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from consultations.models import (
+    ConsultationRequest,
+    SurveyAnswer,
+    ChosenAnswer,
+    ChoiceOption,
+    Question
+)
+
+
+admin.site.register(ConsultationRequest)
+admin.site.register(SurveyAnswer)
+admin.site.register(ChosenAnswer)
+admin.site.register(ChoiceOption)
+admin.site.register(Question)
