1bddd8f9d3de438569be5ba03e86a6ba1f85e128
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-05-04 18:37:13 +0300

    Create ConsultationCreateView with url mapping

--- app/backend/consultations/urls.py
+++ app/backend/consultations/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from consultations.views import ConsultationCreateView
 
-urlpatterns = []
+
+urlpatterns = [
+    path("requests/", ConsultationCreateView.as_view(), name="consultation-create"),
+]
 
 app_name = "consultations"
--- app/backend/consultations/views.py
+++ app/backend/consultations/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from smtplib import SMTPException
 
-# Create your views here.
+from rest_framework import generics
+
+from consultations.models import ConsultationRequest
+from consultations.notifications import send_admin_consultation_notification
+from consultations.serializers import ConsultationRequestSerializer
+
+
+
+class ConsultationCreateView(generics.CreateAPIView):
+    queryset = ConsultationRequest.objects.all()
+    serializer_class = ConsultationRequestSerializer
+
+    def perform_create(self, serializer):
+        consultation = serializer.save()
+
+        try:
+            send_admin_consultation_notification(
+                name=consultation.customer_name,
+                number_phone=consultation.phone_number,
+                created_at=consultation.created_at,
+                question=consultation.customer_question
+            )
+        except SMTPException:
+            pass
