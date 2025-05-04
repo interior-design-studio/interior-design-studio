67a94705ee7e5a7b30eb2e548a06a2b2e85c5118
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-05-04 18:38:27 +0300

    Add documentation to ConsultationCreateView

--- app/backend/consultations/views.py
+++ app/backend/consultations/views.py
@@ -1,5 +1,6 @@
 from smtplib import SMTPException
 
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics
 
 from consultations.models import ConsultationRequest
@@ -7,7 +8,13 @@ from consultations.notifications import send_admin_consultation_notification
 from consultations.serializers import ConsultationRequestSerializer
 
 
-
+@extend_schema(
+    summary="Create a new consultation request",
+    description="Endpoint for submitting a consultation request. "
+                "Can be used on its own or together with a report of answers "
+                "collected from a prior questionnaire.",
+    responses=ConsultationRequestSerializer
+)
 class ConsultationCreateView(generics.CreateAPIView):
     queryset = ConsultationRequest.objects.all()
     serializer_class = ConsultationRequestSerializer
