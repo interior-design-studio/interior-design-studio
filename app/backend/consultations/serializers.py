10bffbe2cfc5476a4171134e8d5adbf5d32ed17b
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-05-04 18:30:32 +0300

    Create ConsultationRequestSerializer

--- app/backend/consultations/serializers.py
+++ app/backend/consultations/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from django.db import transaction
 
-from consultations.models import ChosenAnswer
+from consultations.models import ConsultationRequest, ChosenAnswer
 
 
 class ChosenAnswerSerializer(serializers.ModelSerializer):
@@ -12,3 +13,25 @@ class ChosenAnswerSerializer(serializers.ModelSerializer):
             "question",
             "custom_answer"
         )
+
+
+class ConsultationRequestSerializer(serializers.ModelSerializer):
+    chosen_answers = ChosenAnswerSerializer(many=True)
+
+    class Meta:
+        model = ConsultationRequest
+        fields = (
+            "id",
+            "customer_name",
+            "phone_number",
+            "customer_question",
+            "chosen_answers"
+        )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            answers = validated_data.pop("chosen_answers")
+            cons_request = ConsultationRequest.objects.create(**validated_data)
+            for answer in answers:
+                ChosenAnswer.objects.create(customer_data=cons_request, **answer)
+            return cons_request
