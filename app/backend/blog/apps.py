970b83ea3a329d3abbff1500a2affa54c2261fe9
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-05-19 21:44:25 +0300

    Register signals in blog app

--- app/backend/blog/apps.py
+++ app/backend/blog/apps.py
@@ -6,8 +6,18 @@ class BlogConfig(AppConfig):
     name = "blog"
 
     def ready(self):
-        from utils.signals import register_image_cleanup
+        from utils.signals import (
+            register_image_cleanup,
+            optimize_image_size,
+            delete_replaced_image_signal
+        )
         from blog.models import Article, ArticleComponent
 
         register_image_cleanup(Article, "image")
         register_image_cleanup(ArticleComponent, "image")
+
+        optimize_image_size(Article, "image")
+        optimize_image_size(ArticleComponent, "image")
+
+        delete_replaced_image_signal(Article, "image")
+        delete_replaced_image_signal(ArticleComponent, "image")
