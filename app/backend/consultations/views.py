509827454a5f08fe0ad87e6208f38ba0a1ad3c61
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-05-21 09:03:47 +0300

    Update ConsultationCreateView

--- app/backend/consultations/views.py
+++ app/backend/consultations/views.py
@@ -7,6 +7,7 @@ from consultations.serializers import (
     QuestionListSerializer
 )
 from consultations.tasks import send_message_task
+from consultations.models import ChosenAnswer, Question
 
 
 @extend_schema(
@@ -22,12 +23,22 @@ class ConsultationCreateView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         consultation = serializer.save()
+        answers = []
+        for i, answer in enumerate(consultation.chosen_answers.all()):
+            if answer.option is not None:
+                answers.append((i, answer.option.question.text, answer.option.text))
+            elif answer.question is not None:
+                answers.append((i, answer.question.text, answer.custom_answer))
+        answers_str = ""
+        for i, question, answer in answers:
+            answers_str += f"{i + 1}. {question}  -  {answer}\n"
 
         send_message_task.delay(
             name=consultation.customer_name,
             number_phone=consultation.phone_number,
             created_at=consultation.created_at.strftime("%d.%m.%Y %H:%M"),
-            question=consultation.customer_question
+            question=consultation.customer_question if consultation.customer_question else "-",
+            answers=answers_str if answers_str != "" else None
         )
 
 class QuestionListView(generics.ListAPIView):
