044aa7d0ba020b4e57e256862e487fd2bd097318
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-05-21 09:03:03 +0300

    Add send_telegram_notification to send_message_task and update requirements.txt

--- app/backend/consultations/tasks.py
+++ app/backend/consultations/tasks.py
@@ -1,13 +1,26 @@
 from celery import shared_task
 from smtplib import SMTPException
 
-from consultations.notifications import send_admin_consultation_notification
+from consultations.notifications import (
+    send_admin_consultation_notification, send_telegram_notification
+)
 
 
 @shared_task
 def send_message_task(
-    name: str, number_phone: str, created_at: str, question: str | None = None
+    name: str,
+    number_phone: str,
+    created_at: str,
+    question: str="-",
+    answers: str=None
 ) -> None:
+    send_telegram_notification(
+        name,
+        number_phone,
+        created_at,
+        question,
+        answers
+    )
     try:
         send_admin_consultation_notification(name, number_phone, created_at, question)
     except SMTPException:
