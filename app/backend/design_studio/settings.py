5f44d3b519685b4c0ef10d09cfa49cb9e1e0d13c
88victory88 <victory.otrok@gmail.com>
2025-06-15 20:38:25 +0800

    studio, frontend, celery, redis, certbot - all works

--- app/backend/design_studio/settings.py
+++ app/backend/design_studio/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = os.getenv("STATIC_URL", "/static/")
+STATIC_URL = os.getenv("STATIC_URL", "/backend-static/")
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
